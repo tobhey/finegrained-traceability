21c57161c044b02b914cded89926025b6df5032f
Fei Chen <Fe.Ch@web.de>
2021-05-09 23:40:02 +0200

    Fix attribute placement

--- CodeToRequirementFileLevelTraceLink.py
+++ CodeToRequirementFileLevelTraceLink.py
@@ -5,7 +5,8 @@ class CodeToRequirementFileLevelTraceLink:
         self.req_file
         self.code_file
         self.file_level_similarity
-        self.element_level_trace_links
+        self.method_to_req_element_trace_links
+        self.non_callgraph_code_element_to_req_element_trace_links
         
         
 class CodeElementToRequirementTraceLink:
@@ -14,8 +15,7 @@ class CodeElementToRequirementTraceLink:
         self.code_element
         self.req_file
         self.code_elem_to_req_similarity
-        self.method_to_req_element_trace_links
-        self.non_callgraph_code_element_to_req_element_trace_links
+        self.element_level_trace_links
         
         
 class ElementLevelTraceLink:
