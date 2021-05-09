f2615626b47e34d30d60192aad00284566399983
Fei Chen <Fe.Ch@web.de>
2021-05-09 23:39:26 +0200

    Improve comprehensibility

--- precalculating/TraceLinkPrecalculator.py
+++ precalculating/TraceLinkPrecalculator.py
@@ -6,6 +6,10 @@ import FileUtil
 
 log = logging.getLogger(__name__)
 
+"""
+Use a TraceLinkPrecalculatorBuilder to instantiate TraceLinkPrecalculators
+"""
+
 class TraceLinkPrecalculator(abc.ABC):
     
     def __init__(self, req_dict, code_dict, similarity_function, output_file_name):
@@ -77,60 +81,78 @@ class ElementLevelTraceLinkPrecalculator(TraceLinkPrecalculator):
     REQUIREMENT_ELEMENT_LIST = "requirement_element_list"
     SIMILARITY = "similarity"
 
-    def __init__(self,req_dict, code_method_dict, code_non_cg_element_dict, similarity_function, output_file_name):
+    def __init__(self, req_dict, code_method_dict, code_non_cg_element_dict, similarity_function, output_file_name):
         super(ElementLevelTraceLinkPrecalculator, self).__init__(req_dict, code_method_dict, similarity_function, output_file_name)
         self._code_non_cg_element_dict = code_non_cg_element_dict
         
     def precalculate_trace_links(self):
-        all_code_files = self._precalculate_code_files()
+        all_code_files = self._precalculate_all_code_files()
         if self._output_file_name:
             FileUtil.write_to_json(self._output_file_name, all_code_files)
         return all_code_files
-    
-    def _precalculate_code_files(self):
+
+    def _precalculate_all_code_files(self):
         all_code_files = []
         num_code_files = len(self._code_dict.keys())
         for i, code_file_name in enumerate(self._code_dict):
             log.info(f"precalculating for code file {i + 1} of {num_code_files}")
-            code_file_entry = dict()
-            code_file_entry[self.CODE_FILENAME] = code_file_name
-            code_file_entry[self.METHOD_LIST] = self._precalculate_code_elements(self._code_dict[code_file_name])
-            code_file_entry[self.NON_CG_CODE_ELEMENT_LIST] = self._precalculate_code_elements(self._code_non_cg_element_dict[code_file_name])
-            
+            code_file_entry = self._precalculate_code_file(code_file_name)
             all_code_files.append(code_file_entry)
         
         return all_code_files
+    
+    def _precalculate_code_file(self, code_file_name):
+        code_file_entry = dict()
+        code_file_entry[self.CODE_FILENAME] = code_file_name
+        code_file_entry[self.METHOD_LIST] = self._precalculate_code_elements(self._code_dict[code_file_name])
+        code_file_entry[self.NON_CG_CODE_ELEMENT_LIST] = self._precalculate_code_elements(self._code_non_cg_element_dict[code_file_name])
+        return code_file_entry
 
     def _precalculate_code_elements(self, code_elem_list):
         code_elem_entry_list = []
         num_elems = len(code_elem_list)
         for j, (code_element_key, code_element_vector) in enumerate(code_elem_list):
             log.info(f"code element {j + 1} of {num_elems}")
-            code_elem_entry = dict()
-            code_elem_entry[self.CODE_ELEMENT] = code_element_key
-            code_elem_entry[self.REQUIREMENT_LIST] = self._precalculate_requirement(code_element_vector)
+            code_elem_entry = self._precalculate_code_element(code_element_key, code_element_vector)
             code_elem_entry_list.append(code_elem_entry)
         
         return code_elem_entry_list
 
-    def _precalculate_requirement(self, code_element_vector):
+    def _precalculate_code_element(self, code_element_key, code_element_vector):
+        code_elem_entry = dict()
+        code_elem_entry[self.CODE_ELEMENT] = code_element_key
+        code_elem_entry[self.REQUIREMENT_LIST] = self._precalculate_requirements(code_element_vector)
+        return code_elem_entry
+
+    def _precalculate_requirements(self, code_element_vector):
         requirements_entry_list = []
         for req_file_name in self._req_dict:
-            requirement_entry = dict()
-            requirement_entry[self.REQUIREMENT_FILENAME] = req_file_name
-            requirement_entry[self.REQUIREMENT_ELEMENT_LIST] = self._precalculate_req_elements(code_element_vector, self._req_dict[req_file_name])
+            requirement_entry = self._precalculate_requirement(code_element_vector, req_file_name)
             requirements_entry_list.append(requirement_entry)
         
         return requirements_entry_list
 
+
+    def _precalculate_requirement(self, code_element_vector, req_file_name):
+        requirement_entry = dict()
+        requirement_entry[self.REQUIREMENT_FILENAME] = req_file_name
+        requirement_entry[self.REQUIREMENT_ELEMENT_LIST] = self._precalculate_req_elements(code_element_vector, self._req_dict[req_file_name])
+        return requirement_entry
+
+
     def _precalculate_req_elements(self, code_element_vector, req_elem_list):
         requirement_elem_entry_list = []
         for req_elem in req_elem_list:
-            requirement_elem_entry = dict()
-            requirement_elem_entry[self.REQUIREMENT_ELEMENT] = self._set_element_if_string(req_elem)
-            requirement_elem_entry[self.SIMILARITY] = self._similarity_function(req_elem, code_element_vector)
+            requirement_elem_entry = self._precalculate_requirement_element(code_element_vector, req_elem)
             requirement_elem_entry_list.append(requirement_elem_entry)
         return requirement_elem_entry_list
+
+
+    def _precalculate_requirement_element(self, code_element_vector, req_elem):
+        requirement_elem_entry = dict()
+        requirement_elem_entry[self.REQUIREMENT_ELEMENT] = self._set_element_if_string(req_elem)
+        requirement_elem_entry[self.SIMILARITY] = self._similarity_function(req_elem, code_element_vector)
+        return requirement_elem_entry
 
 
             
--- precalculating/TraceLinkPrecalculatorBuilder.py
+++ precalculating/TraceLinkPrecalculatorBuilder.py
@@ -1,13 +1,14 @@
-from autograd.builtins import isinstance
-from Embedding import RequirementEmbedding
 from precalculating.TraceLinkPrecalculator import FileLevelTraceLinkPrecalculator,\
     ElementLevelTraceLinkPrecalculator
 
 class TraceLinkPrecalculatorBuilder:
-    
-    def __init__(self, req_embeddings, code_embeddings, similarity_function, output_file_path):
-        self._req_embeddings = req_embeddings
-        self._code_embeddings = code_embeddings
+    """
+    A TraceLinkPrecalculatorBuilder takes requirement and class embedding containers and initializes a
+    TraceLinkPrecalculator with them (in order to calculate a precalculated file out of the embeddings).
+    """
+    def __init__(self, _req_embedding_containers, code_embedding_containers, similarity_function, output_file_path):
+        self._req_embedding_containers = _req_embedding_containers
+        self._code_embedding_containers = code_embedding_containers
         self._similarity_function = similarity_function
         self._output_file_path = output_file_path
         
@@ -19,10 +20,10 @@ class FileLevelTraceLinkPrecalculatorBuilder(TraceLinkPrecalculatorBuilder):
     
     def build(self):
         req_dict, code_dict = {}, {}
-        for req in self._req_embeddings:
-            req_dict[req.file_name] = req.file_vector
-        for code in self._code_embeddings:
-            code_dict[code.file_name] = code.file_vector
+        for req_emb_cont in self._req_embedding_containers:
+            req_dict[req_emb_cont.file_name] = req_emb_cont.file_vector
+        for code_emb_cont in self._code_embedding_containers:
+            code_dict[code_emb_cont.file_name] = code_emb_cont.file_vector
         return FileLevelTraceLinkPrecalculator(req_dict, code_dict, self._similarity_function, self._output_file_path)
     
 class ElementLevelTraceLinkPrecalculatorBuilder(TraceLinkPrecalculatorBuilder):
@@ -30,11 +31,11 @@ class ElementLevelTraceLinkPrecalculatorBuilder(TraceLinkPrecalculatorBuilder):
     def build(self):
         req_dict, method_dict, non_cg_code_element_dict = {}, {}, {}
         
-        for req_emb in self._req_embeddings:
-            req_dict[req_emb.file_name] = req_emb.requirement_element_vectors
-        for code_emb in self._code_embeddings:
-            for method_key in code_emb.methods_dict:
-                method_dict[code_emb.file_name] = (method_key, code_emb.methods_dict[method_key])
-            for non_cg_element_key in code_emb.non_cg_dict:
-                non_cg_code_element_dict[code_emb.file_name] = (non_cg_element_key, code_emb.non_cg_dict[non_cg_element_key])
+        for req_emb_cont in self._req_embedding_containers:
+            req_dict[req_emb_cont.file_name] = req_emb_cont.requirement_element_vectors
+        for code_emb_cont in self._code_embedding_containers:
+            for method_key in code_emb_cont.methods_dict:
+                method_dict[code_emb_cont.file_name] = (method_key, code_emb_cont.methods_dict[method_key])
+            for non_cg_element_key in code_emb_cont.non_cg_dict:
+                non_cg_code_element_dict[code_emb_cont.file_name] = (non_cg_element_key, code_emb_cont.non_cg_dict[non_cg_element_key])
         return ElementLevelTraceLinkPrecalculator(req_dict, method_dict, non_cg_code_element_dict, self._similarity_function, self._output_file_name)
