a0f429c33d14d653d97fc8492d2d97bd087d404c
Fei Chen <Fe.Ch@web.de>
2021-06-27 00:17:13 +0200

    Add SimilarityFilter

---
+++ SimilarityFilter.py
@@ -0,0 +1,14 @@
+from abc import ABC, abstractmethod
+
+
+class SimilarityFilter(ABC):
+    
+    def __init__(self, bigger_is_more_similar=True):
+        if bigger_is_more_similar:
+            self._compare_func = lambda a, b : a > b
+        else:
+            self._compare_func = lambda a, b : a < b
+    
+    def is_more_similar(self, data, drop_threshold):
+        return self._compare_func(data, drop_threshold)
+    
