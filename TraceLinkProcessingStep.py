1bed7974836452a5652437719d87673c65a64d98
Fei Chen <Fe.Ch@web.de>
2021-06-27 01:41:33 +0200

    Update TraceLinkProcessors

--- TraceLinkProcessingStep.py
+++ TraceLinkProcessingStep.py
@@ -17,8 +17,8 @@ log = logging.getLogger(__name__)
 
 class TraceLinkProcessingStep(ABC):
     
-    def __init__(self, trace_link_provider):
-        self._trace_link_provider = trace_link_provider
+    def __init__(self, trace_link_data_structure):
+        self._trace_link_data_structure = trace_link_data_structure
     
     
 class FileLevelTraceLinkCreator(TraceLinkProcessingStep):
@@ -36,23 +36,47 @@ class FileLevelTraceLinkCreator(TraceLinkProcessingStep):
         return all_trace_links
 
 
-class ElementLevelTraceLinkAggregator(TraceLinkProcessingStep):
+class MajorityDecisionTraceLinkCreator:
+    
+    def __init__(self, trace_link_provider, similarity_filter, req_reduce_func, code_reduce_function, callgraph_aggregator=None):
+        super(MajorityDecisionTraceLinkCreator, self).__init__(trace_link_provider)
+        self._element_level_trace_link_aggregator = ElementLevelTraceLinkAggregator(req_reduce_func)
+        self._callgraph_aggregator = callgraph_aggregator
+        self._majority_decision = MajorityDecision(similarity_filter, code_reduce_function)
+        
+    def process(self, majority_drop_thresh) -> [TraceLink]:
+        
+        # Step 1: Calculate code element to (whole) req trace links
+        trace_link_data_structure = self._element_level_trace_link_aggregator.process(self._trace_link_data_structure)
+        
+        # Step 2: (optional) Update code element to (whole) req trace links according to call graph neighbors
+        if self._callgraph_aggregator:
+            trace_link_data_structure = self._callgraph_aggregator.process(self._trace_link_data_structure)
 
-    def __init__(self, trace_link_provider, req_reduce_func):
-        assert isinstance(trace_link_provider, TraceLinkProvider)
-        super(ElementLevelTraceLinkAggregator, self).__init__(trace_link_provider)
+        # Step 3: Do majority decision to obtain (whole) code file to (whole) req similarities
+        return self._majority_decision.process(trace_link_data_structure, majority_drop_thresh)
+
+        
+class ElementLevelTraceLinkAggregator:
+
+    """
+    Result: reqfile x code element matrix
+    """
+
+    def __init__(self, req_reduce_func):
         self._req_reduce_func = req_reduce_func
 
-    def process(self) -> TraceLinkProvider:
+    def process(self, trace_link_data_structure) -> TraceLinkProvider:
+        self._trace_link_provider = trace_link_data_structure
         method_to_req_similarities = TwoDimensionalMatrix.create_empty()
         for code_file_name in self._trace_link_provider.all_code_file_names():
-            method_to_req_similarities = self._calculate_reduced_similarity_for_code_element(method_to_req_similarities, self._trace_link_provider.all_code_elements_of(code_file_name))
+            method_to_req_similarities = self._calculate_reduced_similarity_for_each_code_element(method_to_req_similarities, code_file_name)
         
         self._trace_link_provider.set_matrix(method_to_req_similarities)
         return self._trace_link_provider
 
-    def _calculate_reduced_similarity_for_code_element(self, method_to_req_similarities, code_elements):
-        for code_element in code_elements:
+    def _calculate_reduced_similarity_for_each_code_element(self, method_to_req_similarities, code_file_name):
+        for code_element in self._trace_link_provider.all_code_elements_of(code_file_name):
             for req_file_name in self._trace_link_provider.all_req_file_names():
                 code_element_to_req_similarity = self._calculate_reduced_similarity_to_all_reqs(code_element, req_file_name)
                 method_to_req_similarities.set_value(req_file_name, code_element, code_element_to_req_similarity)
@@ -68,47 +92,47 @@ class ElementLevelTraceLinkAggregator(TraceLinkProcessingStep):
         return self._req_reduce_func(all_element_level_similarities_to_same_req)
 
 
-class CallGraphTraceLinkAggregator(TraceLinkProcessingStep):
+class CallGraphTraceLinkAggregator:
     
-    def __init__(self, trace_link_provider, method_weight, neighbor_strategy, method_call_graph_dict):
-        super(CallGraphTraceLinkAggregator, self).__init__(trace_link_provider)
+    def __init__(self, method_weight, neighbor_strategy, method_call_graph_dict):
         self.method_call_graph_dict = method_call_graph_dict
         self.neighbor_handler = NeighborHandler(neighbor_strategy, method_call_graph_dict)
         self.method_weight = method_weight
             
-    def process(self) -> TraceLinkProvider:
+    def process(self, trace_link_data_structure) -> TraceLinkProvider:
+        self._trace_link_data_structure = trace_link_data_structure
         method_to_req_similarity_matrix_with_cg = TwoDimensionalMatrix.create_empty()
-        for method_key in self._trace_link_provider.all_method_keys():
+        for method_key in self._trace_link_data_structure.all_method_keys():
             if not method_key in self.method_call_graph_dict:
                 log.info(f"SKIP: {method_key} is not in the call graph")
             else:
                 all_neighbor_keys = self.neighbor_handler.get_neighbor_method_keys_of(method_key)
-                for req_file_name in self._trace_link_provider.all_req_file_names():
+                for req_file_name in self._trace_link_data_structure.all_req_file_names():
                     weighted_similarity = self._weighted_simialrity_sum_of(method_key, all_neighbor_keys, req_file_name)
                     method_to_req_similarity_matrix_with_cg.set_value(req_file_name, method_key, weighted_similarity)
-        self._trace_link_provider.set_matrix(method_to_req_similarity_matrix_with_cg)
-        return self._trace_link_provider
+        self._trace_link_data_structure.set_matrix(method_to_req_similarity_matrix_with_cg)
+        return self._trace_link_data_structure
     
     def _weighted_simialrity_sum_of(self, method_key, all_neighbor_keys, req_file_name):
-        current_sim = self._trace_link_provider.similarity_between(req_file_name, method_key)
-        neighbor_sims = [self._trace_link_provider.similarity_between(req_file_name, neighbor_key) for neighbor_key in all_neighbor_keys]
+        current_sim = self._trace_link_data_structure.similarity_between(req_file_name, method_key)
+        neighbor_sims = [self._trace_link_data_structure.similarity_between(req_file_name, neighbor_key) for neighbor_key in all_neighbor_keys]
         if not neighbor_sims:  # Method has no neighbors
             return current_sim
         neighbor_weight = (1 - self.method_weight)
         return self.method_weight * current_sim + neighbor_weight * sum(neighbor_sims) / len(neighbor_sims)
 
 
-class MajorityDecision(TraceLinkProcessingStep):
+class MajorityDecision:
 
-    def __init__(self, trace_link_provider, similarity_filter, code_reduce_function):
-        super(MajorityDecision, self).__init__(trace_link_provider)
+    def __init__(self, similarity_filter, code_reduce_function):
         self._similarity_filter = similarity_filter
         self._code_reduce_function = code_reduce_function
         
-    def process(self, majority_drop_thresh) -> [TraceLink]:
+    def process(self, trace_link_data_structure, majority_drop_thresh) -> [TraceLink]:
+        self._trace_link_data_structure = trace_link_data_structure
         # One majority decision per code file (assumption: one top level class per code file)
         resulting_trace_links = []
-        for code_file_name in self._trace_link_provider.all_code_file_names:
+        for code_file_name in self._trace_link_data_structure.all_code_file_names:
             votes, sims_per_req = self._collect_votes_and_similarities(majority_drop_thresh, code_file_name)
             resulting_trace_links += self._do_majority_decision(code_file_name, votes, sims_per_req)
         return resulting_trace_links
@@ -116,9 +140,9 @@ class MajorityDecision(TraceLinkProcessingStep):
     def _collect_votes_and_similarities(self, majority_drop_thresh, code_file_name):
         votes = []
         sims_per_req = MajorityDecision.AppendValueDict()
-        for code_elem in self._trace_link_provider.all_code_elements_of(code_file_name):
-            for req_file_name in self._trace_link_provider.all_req_file_names():
-                similarity = self._trace_link_provider.similarity_between(req_file_name, code_elem)
+        for code_elem in self._trace_link_data_structure.all_code_elements_of(code_file_name):
+            for req_file_name in self._trace_link_data_structure.all_req_file_names():
+                similarity = self._trace_link_data_structure.similarity_between(req_file_name, code_elem)
                 if (self._similarity_filter.is_more_similar(similarity, majority_drop_thresh)):
                     votes.append(req_file_name)
                     sims_per_req.append(req_file_name, similarity)
--- TraceLinkProcessor2.py
+++ TraceLinkProcessor2.py
@@ -1,49 +1,45 @@
-from test.mp_preload import proc
 
-from TraceLink import normalize_value_range
-from TraceLinkProcessingStep import MajorityDecision, FileLevelTraceLinkCreator
-from precalculating.AllTraceLinkCombinations import TraceLinkProvider
+from TraceLinkProcessingStep import MajorityDecision, FileLevelTraceLinkCreator, \
+    MajorityDecisionTraceLinkCreator
 
 
-class TraceLinkProcessor2:
+class TraceLinkFilter:
     
-    def __init__(self, file_level_trace_links, normalize=False):
-        if normalize:
-            self._file_level_trace_links = normalize_value_range(file_level_trace_links)
-        else:
-            self._file_level_trace_links = file_level_trace_links
+    def __init__(self, similarity_filter, normalize=False):
+        self._similarity_filter = similarity_filter
+        # Hier würde normalisiert werden
+        pass
     
-    def run(self, file_level_thresholds):
+    def run(self, trace_links, drop_thresholds):
         """
         Returns a dictionary. File level threshold as keys, filtered trace link list as value
         """
-        filtered_file_level_trace_links = {}
-        for file_level_thresh in file_level_thresholds:
-            filtered_file_level_trace_links[file_level_thresh] = self._apply_file_level_threshold(self._file_level_trace_links, file_level_thresh)
-        return filtered_file_level_trace_links
+        filtered_trace_links = {}
+        for thresh in drop_thresholds:
+            filtered_trace_links[thresh] = self._apply_threshold(trace_links, thresh)
+        return filtered_trace_links
             
-    def _apply_file_level_threshold(self, trace_links, drop_thresh):
-        if self.is_reverse_compare():
-            return [trace_link for trace_link in trace_links if trace_link.similarity <= drop_thresh]
-        else:
-            return [trace_link for trace_link in trace_links if trace_link.similarity > drop_thresh]
+    def _apply_threshold(self, trace_links, drop_thresh):
+        return [self._similarity_filter(trace_link.similarity, drop_thresh) for trace_link in trace_links]
 
             
 class FileLevelProcessor:
 
-    def __init__(self, trace_link_provider, file_level_thresholds):
-        self._maj_decision = FileLevelTraceLinkCreator(trace_link_provider)
+    def __init__(self, trace_link_provider, similarity_filter, file_level_thresholds):
+        self._trace_link_creator = FileLevelTraceLinkCreator(trace_link_provider)
+        self._file_level_filter = TraceLinkFilter(similarity_filter)
         self._file_level_thresholds = file_level_thresholds
         
     def run(self):
-        filtered_file_level_trace_links = self.trace_processor.run(file_level_thresholds)
-        valid_trace_links = self._evaluate_tracelinks(filtered_file_level_trace_links)
+        file_level_trace_links = self._trace_link_creator.process()
+        return self._file_level_filter.run(file_level_trace_links, self._file_level_thresholds)
 
 
 class MajProcessor:
 
-    def __init__(self, trace_link_provider, similarity_filter, code_reduce_function, file_level_thresholds, maj_thresholds):
-        self._maj_decision = MajorityDecision(trace_link_provider, similarity_filter, code_reduce_function)
+    def __init__(self, trace_link_provider, similarity_filter, req_reduce_func, code_reduce_function, file_level_thresholds, maj_thresholds, callgraph_aggregator=None):
+        self._maj_decision = MajorityDecisionTraceLinkCreator(trace_link_provider, similarity_filter, req_reduce_func, code_reduce_function, callgraph_aggregator)
+        self._file_level_filter = TraceLinkFilter(similarity_filter)
         self._maj_thresholds = maj_thresholds
         self._file_level_thresholds = file_level_thresholds
     
@@ -51,7 +47,5 @@ class MajProcessor:
         processed_trace_links = {}
         for maj_threshold in self._maj_thresholds:
             file_level_trace_links = self._maj_decision.process(maj_threshold)
-            processed_trace_links[maj_threshold] = TraceLinkProcessor2(file_level_trace_links, self._normalize).run(self._file_level_thresholds)
-            # eval auslagern?
-            # valid_trace_links = self._evaluate_tracelinks(filtered_file_level_trace_links)
+            processed_trace_links[maj_threshold] = self._file_level_filter.run(file_level_trace_links, self._file_level_thresholds)
         return processed_trace_links
