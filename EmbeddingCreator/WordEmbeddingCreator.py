a912c4707ffa540512daa82ea5d95ddbc2bbfaa5
Fei Chen <Fe.Ch@web.de>
2021-06-27 19:26:41 +0200

    Rename to RandomWordEmbeddingCreator

--- EmbeddingCreator/WordEmbeddingCreator.py
+++ EmbeddingCreator/WordEmbeddingCreator.py
@@ -1,12 +1,15 @@
 from abc import ABC, abstractmethod
-import gensim.models.wrappers
 import logging
-import Util
 import random
+
+import gensim.models.wrappers
+
+import Util
 
 COLAB_MODEL_PATH = "/content/drive/My Drive/models/cc.en.300.bin"
 
 log = logging.getLogger(__name__)
+
 
 class WordEmbeddingCreator(ABC) :
     
@@ -23,14 +26,16 @@ class WordEmbeddingCreator(ABC) :
         Calculates the word mover's distance between two bag of words
         """
         pass
+
     
 class FastTextEmbeddingCreator(WordEmbeddingCreator):
     """
     Use this for monolingual fasttext models that uses .bin models
     """
+
     def __init__(self, model_path=COLAB_MODEL_PATH):
         self._model = gensim.models.wrappers.FastText.load_fasttext_format(model_path)
-        self._model.init_sims(replace=True) # normalizes vectors tp length 1
+        self._model.init_sims(replace=True)  # normalizes vectors to length 1
 
     def create_word_embedding(self, word):
         try:
@@ -41,13 +46,15 @@ class FastTextEmbeddingCreator(WordEmbeddingCreator):
     
     def word_movers_distance(self, str_list_1, str_list_2):
         return self._model.wmdistance(str_list_1, str_list_2)
+
      
-class MockWordEmbeddingCreator(WordEmbeddingCreator):
+class RandomWordEmbeddingCreator(WordEmbeddingCreator):
     """
     Creates random embedding vectors
     For testing purposes since initializing fastText model is expensive
     """
-    def __init__(self, seed = None):
+
+    def __init__(self, seed=None):
         if seed:
             random.seed(seed)
     
@@ -57,5 +64,5 @@ class MockWordEmbeddingCreator(WordEmbeddingCreator):
     def word_movers_distance(self, str_list_1, str_list_2):
         assert isinstance(str_list_1, list)
         assert isinstance(str_list_2, list)
-        return random.uniform(0, 10)
+        return random.uniform(0, 2)
     
