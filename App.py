63ae797763a7c6365ac0d58687140549e2e5982a
Fei Chen <Fe.Ch@web.de>
2021-06-28 23:39:18 +0200

    Prepare for colab run

--- App.py
+++ App.py
@@ -1,5 +1,19 @@
-from Dataset import Etour
+from Dataset import Etour, Itrust, Smos, EANCI
 from TraceabilityBuilder import BaseLineRunner
+import Util
 
+F = [0.44]  # Util.get_range_array(0.1, 0.8, 0.1)
+M = [0.59]  # Util.get_range_array(0.3, 0.7, 0.1)
 b = BaseLineRunner(Etour())
 b.precalculate()
+b.calculate_f1(F, M)
+
+b = BaseLineRunner(Itrust())
+b.precalculate()
+b.calculate_f1(F, M)
+b = BaseLineRunner(Smos())
+b.precalculate()
+b.calculate_f1(F, M)
+b = BaseLineRunner(EANCI())
+b.precalculate()
+b.calculate_f1(F, M)
