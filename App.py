34488de283813891465348f6cf726618221453d8
Fei Chen <Fe.Ch@web.de>
2021-07-01 00:15:10 +0200

    Prepare for Run

--- App.py
+++ App.py
@@ -1,27 +1,28 @@
 from Dataset import Etour, Itrust, Smos, EANCI
-from TraceabilityBuilder import BaseLineRunner, BaseLineCDRunner
+from TraceabilityBuilder import BaseLineRunner, BaseLineCDRunner, \
+    BaseLineUCTRunner, BaseLineMCRunner
 import Util
 
 F = [0.44]  # Util.get_range_array(0.1, 0.8, 0.1)
 M = [0.59]  # Util.get_range_array(0.3, 0.7, 0.1)
 
-b = BaseLineCDRunner(Etour())
-b.precalculate()
-b.calculate_f1(F, M)
-b.calculate_map()
-
-b = BaseLineRunner(Itrust())
+b = BaseLineMCRunner(Etour())
 b.precalculate()
 b.calculate_map()
-b.calculate_f1(F, M)
-
-b = BaseLineRunner(Smos())
-b.precalculate()
+# b.calculate_f1(F, M)
+"""
+b = BaseLineCDRunner(Itrust())
+# b.precalculate()
 b.calculate_map()
 b.calculate_f1(F, M)
 
-b = BaseLineRunner(EANCI())
-b.precalculate()
+b = BaseLineCDRunner(Smos())
+# b.precalculate()
 b.calculate_map()
 b.calculate_f1(F, M)
 
+b = BaseLineCDRunner(EANCI())
+# b.precalculate()
+b.calculate_map()
+b.calculate_f1(F, M)
+"""
