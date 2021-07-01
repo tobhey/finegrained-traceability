adf1a42bf54e0f515e5eed432b154f548cdb0248
Fei Chen <Fe.Ch@web.de>
2021-07-01 23:57:18 +0200

    Setup for run

--- App.py
+++ App.py
@@ -3,29 +3,30 @@ import pandas
 from Dataset import Etour, Itrust, Smos, EANCI
 import PandasUtil
 from TraceabilityBuilder import BaseLineRunner, BaseLineCDRunner, \
-    BaseLineUCTRunner, BaseLineMCRunner
+    BaseLineUCTRunner, BaseLineMCRunner, \
+    BaseLineMCUCTRunner, BaseLineUCTCDRunner, BaseLineMCUCTCDRunner
 import Util
 
 F = [0.44]  # Util.get_range_array(0.1, 0.8, 0.1)
 M = [0.59]  # Util.get_range_array(0.3, 0.7, 0.1)
 
-b = BaseLineUCTRunner(Etour())
-b.precalculate()
-b.calculate_map()
-b.calculate_f1(F, M)
-"""
-b = BaseLineUCTRunner(Itrust())
+b = BaseLineMCUCTRunner(Etour())
 # b.precalculate()
 b.calculate_map()
 b.calculate_f1(F, M)
-"""
-b = BaseLineUCTRunner(Smos())
-b.precalculate()
+""""""
+b = BaseLineMCUCTRunner(Itrust())
+# b.precalculate()
 b.calculate_map()
 b.calculate_f1(F, M)
 
-b = BaseLineUCTRunner(EANCI())
-b.precalculate()
+b = BaseLineMCUCTRunner(Smos())
+# b.precalculate()
+b.calculate_map()
+b.calculate_f1(F, M)
+
+b = BaseLineMCUCTRunner(EANCI())
+# b.precalculate()
 b.calculate_map()
 b.calculate_f1(F, M)
 """"""
