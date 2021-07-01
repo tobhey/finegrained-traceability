21db44cbaac6279abec01d750a5fc91661bfc0e4
Fei Chen <Fe.Ch@web.de>
2021-07-01 21:33:55 +0200

    Setup for BaseLineUCTRunner

--- App.py
+++ App.py
@@ -1,4 +1,7 @@
+import pandas
+
 from Dataset import Etour, Itrust, Smos, EANCI
+import PandasUtil
 from TraceabilityBuilder import BaseLineRunner, BaseLineCDRunner, \
     BaseLineUCTRunner, BaseLineMCRunner
 import Util
@@ -6,23 +9,23 @@ import Util
 F = [0.44]  # Util.get_range_array(0.1, 0.8, 0.1)
 M = [0.59]  # Util.get_range_array(0.3, 0.7, 0.1)
 
-b = BaseLineMCRunner(Etour())
+b = BaseLineUCTRunner(Etour())
 b.precalculate()
 b.calculate_map()
-# b.calculate_f1(F, M)
+b.calculate_f1(F, M)
 """
-b = BaseLineCDRunner(Itrust())
-# b.precalculate()
-b.calculate_map()
-b.calculate_f1(F, M)
-
-b = BaseLineCDRunner(Smos())
-# b.precalculate()
-b.calculate_map()
-b.calculate_f1(F, M)
-
-b = BaseLineCDRunner(EANCI())
+b = BaseLineUCTRunner(Itrust())
 # b.precalculate()
 b.calculate_map()
 b.calculate_f1(F, M)
 """
+b = BaseLineUCTRunner(Smos())
+b.precalculate()
+b.calculate_map()
+b.calculate_f1(F, M)
+
+b = BaseLineUCTRunner(EANCI())
+b.precalculate()
+b.calculate_map()
+b.calculate_f1(F, M)
+""""""
