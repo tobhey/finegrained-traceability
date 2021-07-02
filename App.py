66613303148989d2b1d481ed256b1296132a2ddb
Fei Chen <Fe.Ch@web.de>
2021-07-02 13:56:41 +0200

    Test

--- App.py
+++ App.py
@@ -4,29 +4,33 @@ from Dataset import Etour, Itrust, Smos, EANCI
 import PandasUtil
 from TraceabilityBuilder import BaseLineRunner, BaseLineCDRunner, \
     BaseLineUCTRunner, BaseLineMCRunner, \
-    BaseLineMCUCTRunner, BaseLineUCTCDRunner, BaseLineMCUCTCDRunner
+    BaseLineUCTMCRunner, BaseLineUCTCDRunner, BaseLineUCTMCCDRunner, \
+    BaseLineMCCDRunner
 import Util
 
-F = [0.44]  # Util.get_range_array(0.1, 0.8, 0.1)
-M = [0.59]  # Util.get_range_array(0.3, 0.7, 0.1)
+ENGLISH_FASTTEXT_MODEL_PATH = "/content/drive/My Drive/models/cc.en.300.bin"
+ITALIAN_FASTTEXT_MODEL_PATH = "/content/drive/My Drive/models/cc.it.300.bin"
 
-b = BaseLineMCUCTRunner(Etour())
-# b.precalculate()
+FINAL_THRESHOLDS = [0.44]  # Util.get_range_array(0.1, 0.8, 0.1)
+MAJORITY_THRESHOLDS = [0.59]  # Util.get_range_array(0.3, 0.7, 0.1)
+
+b = BaseLineRunner(Etour())
+# b.precalculate(ENGLISH_FASTTEXT_MODEL_PATH)
 b.calculate_map()
-b.calculate_f1(F, M)
+# b.calculate_f1(FINAL_THRESHOLDS, MAJORITY_THRESHOLDS)
+"""
+b = BaseLineMCCDRunner(Itrust())
+# b.precalculate(ENGLISH_FASTTEXT_MODEL_PATH)
+b.calculate_map()
+b.calculate_f1(FINAL_THRESHOLDS, MAJORITY_THRESHOLDS)
 """"""
-b = BaseLineMCUCTRunner(Itrust())
-# b.precalculate()
+b = BaseLineMCCDRunner(Smos())
+# b.precalculate(ITALIAN_FASTTEXT_MODEL_PATH)
 b.calculate_map()
-b.calculate_f1(F, M)
+b.calculate_f1(FINAL_THRESHOLDS, MAJORITY_THRESHOLDS)
 
-b = BaseLineMCUCTRunner(Smos())
-# b.precalculate()
+b = BaseLineMCCDRunner(EANCI())
+# b.precalculate(ITALIAN_FASTTEXT_MODEL_PATH)
 b.calculate_map()
-b.calculate_f1(F, M)
-
-b = BaseLineMCUCTRunner(EANCI())
-# b.precalculate()
-b.calculate_map()
-b.calculate_f1(F, M)
-""""""
+b.calculate_f1(FINAL_THRESHOLDS, MAJORITY_THRESHOLDS)
+"""
