e3c0dfca0bfd1debabf094878b9530b85f43757c
Tobias Hey <hey@kit.edu>
2021-11-12 17:20:29 +0100

    fix test files of etour

--- datasets/eTour/code/NewsTableModelTest.java
+++ datasets/eTour/code/NewsTableModelTest.java
@@ -1,11 +1,11 @@
-/ **
-  * Class tests for NewsTableModel
-  *
-  * @ Author Mario Gallo
-  * @ Version 0.1
-  *
-  *  2007 eTour Project - Copyright by DMI SE @ SA Lab - University of Salerno
-  * /
+/**
+ * Class tests for NewsTableModel
+ *
+ * @ Author Mario Gallo
+ * @ Version 0.1
+ *
+ *  2007 eTour Project - Copyright by DMI SE @ SA Lab - University of Salerno
+ */
 package unisa.gps.etour.gui.operatoreagenzia.tables.test;
 
 import java.util.ArrayList;
@@ -15,178 +15,178 @@ import unisa.gps.etour.bean.BeanNews;
 import unisa.gps.etour.gui.operatoreagenzia.tables.NewsTableModel;
 import junit.framework.TestCase;
 
-public class TestCase extends NewsTableModelTest
-(
+public class NewsTableModelTest
+{
 
 private NewsTableModel TableModel;
-Private BeanNews aNews;
-Private BeanNews aNewsModificata;
+private BeanNews aNews;
+private BeanNews aNewsModificata;
 
 public NewsTableModelTest (String pName)
-(
+{
 super (pName);
 aNews = new BeanNews ( "An example of news", new Date (), new Date (), 2,1);
 aNewsModificata = new BeanNews ( "A news amended sample", new Date (), new Date (), 3,1);
-)
+}
 
 protected void setUp () throws Exception
-(
+{
 super.setUp ();
 NewsTableModel = new TableModel ();
-)
+}
 
-/ *
-* Verify the behavior of the manufacturer with an ArrayList of BeanNews.
-* /
+/*
+ * Verify the behavior of the manufacturer with an ArrayList of BeanNews.
+ */
 public void testCostruttoreConArrayList ()
-(
+{
 <BeanNews> ArrayList test = new ArrayList <BeanNews> ();
 for (int i = 0; i <10; i + +)
-(
+{
 test.add (new BeanNews ( "text" + i, new Date (), new Date (), 5, i));
-)
+}
 NewsTableModel = new TableModel (test);
 for (int i = 0; i <10; i + +)
-(
+{
 assertSame (test.get (i). getId (), tableModel.getID (i));
-)
+}
 
-)
+}
 
-/ *
-* Verify the manufacturer with an ArrayList Compor zero.
-* /
+/*
+ * Verify the manufacturer with an ArrayList Compor zero.
+ */
 public void testCostruttoreConArrayListNull ()
-(
+{
 NewsTableModel = new TableModel (null);
-)
+}
 
-/ *
-* Verify the behavior of the manufacturer with an empty ArrayList.
-* /
+/*
+ * Verify the behavior of the manufacturer with an empty ArrayList.
+ */
 public void testCostruttoreConArrayListVuoto ()
-(
+{
 NewsTableModel = new TableModel (<BeanNews> new ArrayList ());
-)
+}
 
-/ *
-* Verify the behavior of the method with the correct parameters.
-* /
+/*
+ * Verify the behavior of the method with the correct parameters.
+ */
 public void testGetValueAtParametriCorretti ()
-(
-/ / Put bean in two model test.
+{
+// Put bean in two model test.
 tableModel.insertNews (aNews);
 tableModel.insertNews (aNewsModificata);
 
-/ / Verify the data entered.
+// Verify the data entered.
 assertSame (aNews.getNews (), tableModel.getValueAt (0, 0));
 assertSame (aNews.getPriorita (), tableModel.getValueAt (0, 1));
 assertSame (aNewsModificata.getNews (), tableModel.getValueAt (1, 0));
 assertSame (aNewsModificata.getPriorita (), tableModel.getValueAt (1, 1));
-)
+}
 
-/ *
-* Verify Compor the method with an index row fold.
-* /
+/*
+ * Verify Compor the method with an index row fold.
+ */
 public void testGetValueAtRigaSballata ()
-(
-TRY
-(
+{
+try
+{
 tableModel.getValueAt (12, 0);
 fail ( "Should be thrown");
-)
+}
 catch (IllegalArgumentException success)
-(
-)
-)
+{
+}
+}
 
-/ *
-* Verify Compor of the method with a column index busted.
-* /
+/*
+ * Verify Compor of the method with a column index busted.
+ */
 public void testGetValueAtColonnaSballata ()
-(
-TRY
-(
+{
+try
+{
 tableModel.getValueAt (0, -121334);
 fail ( "Should be thrown");
-)
+}
 catch (IllegalArgumentException success)
-(
-)
-)
+{
+}
+}
 
-/ *
-* Verify Compor method with proper parameter.
-* /
+/*
+ * Verify Compor method with proper parameter.
+ */
 public void testInsertNewsParametroCorretto ()
-(
+{
 tableModel.insertNews (aNews);
 assertSame (aNews.getId (), tableModel.getID (0));
-)
+}
 
-/ *
-* Verify Compor method with parameter to null
-* /
+/*
+ * Verify Compor method with parameter to null
+ */
 public void testInsertNewsParametroNull ()
-(
-TRY
-(
+{
+try
+{
 tableModel.insertNews (null);
 fail ( "Should be thrown");
-)
+}
 catch (IllegalArgumentException success)
-(
-)
-)
+{
+}
+}
 
-/ *
-* Verify Compor method with proper parameter.
-* /
+/*
+ * Verify Compor method with proper parameter.
+ */
 public void testUpdateNewsParametroCorretto ()
-(
+{
 tableModel.insertNews (aNews);
 tableModel.updateNews (aNewsModificata);
 assertSame (aNewsModificata.getNews (), tableModel.getValueAt (0, 0));
 assertSame (aNewsModificata.getPriorita (), tableModel.getValueAt (0, 1));
 assertSame (aNewsModificata.getId (), tableModel.getID (0));
-)
+}
 
-/ *
-* Verify Compor method with parameter to null
-* /
+/*
+ * Verify Compor method with parameter to null
+ */
 public void testUpdateNewsParametroNull ()
-(
+{
 
-TRY
-(
+try
+{
 tableModel.updateNews (null);
 fail ( "Should be thrown");
-)
+}
 catch (IllegalArgumentException success)
-(
-)
-)
+{
+}
+}
 
-/ *
-* Verify Compor method with proper parameter.
-* /
+/*
+ * Verify Compor method with proper parameter.
+ */
 public void testRemoveNewsParametroCorretto ()
-(
+{
 tableModel.insertNews (aNews);
 assertSame (aNews.getId (), tableModel.removeNews (0));
-)
+}
 
-/ *
-* Verify Compor of the method with row index busted.
-* /
+/*
+ * Verify Compor of the method with row index busted.
+ */
 public void testRemoveNewsRigaSballata ()
-(
-TRY
-(
+{
+try
+{
 tableModel.removeNews (-1231);
 fail ( "Should be thrown");
-)
+}
 catch (IllegalArgumentException success) ()
-)
+}
 
-) 
+} 
--- datasets/eTour/code/TestoNewsRendererTest.java
+++ datasets/eTour/code/TestoNewsRendererTest.java
@@ -1,10 +1,10 @@
-/ **
-  * Test case for class TestoNewsRenderer
-  *
-  * @ Author Mario Gallo
-  * @ Version 0.1  2007 eTour Project - Copyright by DMI SE @ SA Lab --
-  * University of Salerno
-  * /
+/**
+ * Test case for class TestoNewsRenderer
+ *
+ * @ Author Mario Gallo
+ * @ Version 0.1  2007 eTour Project - Copyright by DMI SE @ SA Lab --
+ * University of Salerno
+ */
 package unisa.gps.etour.gui.operatoreagenzia.tables.test;
 
 import java.util.Date;
@@ -15,16 +15,16 @@ import unisa.gps.etour.gui.operatoreagenzia.tables.NewsTableModel;
 import unisa.gps.etour.gui.operatoreagenzia.tables.TestoNewsRenderer;
 import junit.framework.TestCase;
 
-public class TestCase extends TestoNewsRendererTest
-(
+public class TestoNewsRendererTest 
+{
 
-Private TestoNewsRenderer renderer;
-Private BeanNews aNewsAttiva;
-Private BeanNews aNewsScaduta;
+private TestoNewsRenderer renderer;
+private BeanNews aNewsAttiva;
+private BeanNews aNewsScaduta;
 private JTable aTable;
 
-public TestoNewsRendererTest ()
-(
+public TestoNewsRendererTest () 
+{
 super ();
 renderer = new TestoNewsRenderer ();
 aNewsAttiva = new BeanNews ( "Here's a news active", new Date (),
@@ -32,79 +32,79 @@ new Date (120, 1, 1), 5, 0);
 aNewsScaduta = new BeanNews ( "Here's a news Expired", new Date (),
 new Date (), 5, 0);
 aTable = new JTable (new NewsTableModel ());
-)
+}
 
-/ *
-* Verify the behavior of the method with the correct parameters.
-* /
-public void testGetTableCellRendererParametriCorretti ()
-(
+/*
+ * Verify the behavior of the method with the correct parameters.
+ */
+public void testGetTableCellRendererParametriCorretti () 
+{
 NewsTableModel aModele = (NewsTableModel) aTable.getModel ();
 aModel.insertNews (aNewsAttiva);
 aModel.insertNews (aNewsScaduta);
 
-/ / Test the renderer with a news active.
+// Test the renderer with a news active.
 JTextArea aare = (JTextArea) renderer.getTableCellRendererComponent (
 aTable, "Here's a news active", true, true, 0, 0);
 assertEquals (aNewsAttiva.getNews (), aArea.getText ());
 
-/ / Test the renderer with a news expired.
+// Test the renderer with a news expired.
 aare = (JTextArea) renderer.getTableCellRendererComponent (aTable,
 "Here's a news Expired", true, true, 0, 0);
 assertEquals (aNewsScaduta.getNews (), aArea.getText ());
 
-)
+}
 
-/ *
-* Verification Compor the table with a table without NewsTableModel
-* Associated.
-* /
+/*
+ * Verification Compor the table with a table without NewsTableModel
+ * Associated.
+ */
 public void testGetTabelCellRendererNoNewsModel ()
-(
+{
 
 JTable anotherTable = new JTable ();
-TRY
-(
+try
+{
 renderer.getTableCellRendererComponent (anotherTable,
 "Here's a news", true, true, 0, 0);
 fail ( "Should be thrown.");
-)
+}
 catch (IllegalArgumentException success)
-(
-)
-)
+{
+}
+}
 
-/ *
-* Verify the behavior of the method with a parameter to null.
-* /
+/*
+ * Verify the behavior of the method with a parameter to null.
+ */
 public void testGetTableCellRendererParametroNull ()
-(
-TRY
-(
+{
+try
+{
 renderer.getTableCellRendererComponent (aTable, null, true, true, 0,
 0);
 fail ( "Should be thrown.");
-)
+}
 catch (IllegalArgumentException success)
-(
-)
-)
+{
+}
+}
 
-/ *
-* Verify the behavior of the method with a data type unexpected.
-* /
+/*
+ * Verify the behavior of the method with a data type unexpected.
+ */
 public void testGetTableCellRendererTipoInatteso ()
-(
-TRY
-(
+{
+try
+{
 renderer
 . getTableCellRendererComponent (aTable, 12, true, true, 0, 0);
 fail ( "Should be thrown.");
-)
+}
 catch (IllegalArgumentException success)
-(
-)
+{
+}
 
-)
+}
 
-)
+}
